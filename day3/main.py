776461e6de5a7fdcd24cf609d343ad7a1dec4ab8
David Laessker <davidlaessker@gmail.com>
2022-12-03 07:25:42 +0100

    Improved day3 code

--- day3/main.py
+++ day3/main.py
@@ -1,28 +1,32 @@
 import string
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', \
+'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',  \
+'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',  \
+'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-# Might make good later but today we went a bit speedy
-
-def sol1():
-    liststore = []
-    value = 0
+def sol():
+    sacks = []
     with open('data.txt') as f:
         for line in f.readlines():
             li = list(line.strip('\n'))
-            liststore += [li]
-            mid = len(li)//2
-            first = li[:mid]
-            second = li[mid:]
-            common = list(set(first).intersection(second))
-            for letter in common:
-                value += (letters.index(letter)+1)
-    newvalue = 0
+            sacks += [li]
 
-    for i in range(0,len(liststore),3):
-        common = list(set(liststore[i]) & set(liststore[i+1]) & set(liststore[i+2]))
-        for letter in common:
-            newvalue += (letters.index(letter)+1)
+    sol1val = 0
+    for li in sacks:
+        mid = len(li)//2
+        first, second = li[:mid], li[mid:]
+        common = list(set(first) & set(second))
+        sol1val += lettersvalue(common)
 
-    print(value)
-    print(newvalue)
+    sol2val = 0
+    for i in range(0,len(sacks),3):
+        common = list(set(sacks[i]) & set(sacks[i+1]) & \
+                                          set(sacks[i+2]))
+        sol2val += lettersvalue(common)
+
+    print('sol1', sol1val)
+    print('sol2', sol2val)
+
+def lettersvalue(common):
+    return sum(((letters.index(letter)+1) for letter in common))
