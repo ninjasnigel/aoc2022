8b2b608df14a5c77e6561fc2719b78d0d1e5e203
David Laessker <davidlaessker@gmail.com>
2022-12-05 06:43:13 +0100

    small fix

--- day5/main.py
+++ day5/main.py
@@ -1,12 +1,10 @@
 import copy
 stacks = []
 
-
 def sol():
     with open("data.txt", "r") as f:
         data = [line.strip('\n') for line in f.readlines()]
     stacks = []
-
     stacks += [list('wbgzrdcv')]
     stacks += [list('vtsbcfwg')]
     stacks += [list('wnsbc')]
@@ -16,30 +14,28 @@ def sol():
     stacks += [list('gtsclfp')]
     stacks += [list('zdb')]
     stacks += [list('wznm')]
+    stacks2 = copy.deepcopy(stacks) # Prob 2 sack
 
-    stacks2 = copy.deepcopy(stacks)
     for line in data:
-        try:
-            if ((line[0] == '[' or line[0] == ' ')):
-                continue
-        except:
-            continue
+        # Read instructions
         ins = list(line.split(' '))
         stack1, stack2, times = int(ins[3])-1, int(ins[5])-1, int(ins[1])
-        print(stack1, stack2, times)
+
+        # Problem 1
         for i in range(times):
             stacks[stack2].insert(0, stacks[stack1].pop(0))
 
+        # Problem 2
         newstack = stacks2[stack1][:times]
         stacks2[stack1] = stacks2[stack1][times:]
         stacks2[stack2] = newstack + stacks2[stack2]
 
-    for i in range(9):
+    for i in range(9): #prob 1
         print(stacks[i][0])
 
     print('----------')
 
-    for i in range(9):
+    for i in range(9): # prob 2
         print(stacks2[i][0])
 
 if __name__ == "__main__":
